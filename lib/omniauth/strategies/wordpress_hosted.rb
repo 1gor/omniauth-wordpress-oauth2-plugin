8fe276a5f253c2bbb5b4e1700e13bf693f7657a5
1gor <1gor@users.noreply.github.com>
2019-04-19 12:12:34 +0500

    Update wordpress_hosted.rb

--- lib/omniauth/strategies/wordpress_hosted.rb
+++ lib/omniauth/strategies/wordpress_hosted.rb
@@ -31,6 +31,10 @@ module OmniAuth
         { :raw_info => raw_info }
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
+      
       def raw_info
         puts access_token.token
         @raw_info ||= access_token.get(
